806efb31ce9ff95944f1066929dac523c4d1b0e4
Milk Lee <milk.lee@qq.com>
2015-12-29 10:08:54 +0800

    添加 uuid 测试

--- tests/libs/uuid-spec.js
+++ tests/libs/uuid-spec.js
@@ -1,7 +1,26 @@
 import uuid from '../../libs/uuid';
 
-describe( 'test' , ()=> {
+describe( 'uuid 函数' , ()=> {
   it( '返回一个 string' , ()=> {
     expect( typeof uuid() ).toBe( 'string' );
+  } );
+
+  it( '多次返回的值都不相同' , ()=> {
+    const m = [];
+    for ( let i = 0 ; i < 10000 ; i += 1 ) {
+      m[ i ] = uuid();
+    }
+
+    const map = {};
+
+    m.some( ( v )=> {
+      if ( map[ v ] ) {
+        fail( 'uuid 的返回值中有重复元素：' + v );
+        return true;
+      } else {
+        map[ v ] = 1;
+      }
+    } );
+
   } );
 } );
